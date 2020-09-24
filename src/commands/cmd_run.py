eb62bbfbae2840675eab21e89f971ccfb59cc7b9
Tucker McCoy <tuckermmccoy@gmail.com>
2020-09-24 08:52:00 -0400

    Update cmd_run.py

--- src/commands/cmd_run.py
+++ src/commands/cmd_run.py
@@ -3,6 +3,7 @@ import os
 
 """Third party modules"""
 import click
+from colorama import Fore
 
 """Internal application modules"""
 from src.main import pass_environment
@@ -26,9 +27,6 @@ from src.lib.subprocess import Command
 def cli(ctx, path, var):
     """Run the tests for the Terraform modules and files"""
 
-    # Total count for successful and failed tests
-    result_total = 0
-
     # Ensure path provided to Terraform directory exists
     tf_directory = os.path.join(path)
     if not os.path.exists(tf_directory):
@@ -51,18 +49,41 @@ def cli(ctx, path, var):
 
     # Run terraform init command to initialize a working directory
     init_result = tf_init(ctx, tf_command, tf_directory, var)
-    result_total += init_result
 
     # Run terraform/terragrunt apply command to apply new resources or changes
     apply_result = tf_apply(ctx, tf_command, tf_directory, var)
-    result_total += apply_result
 
     # Run terraform/terragrunt destroy to destroy the Terraform-managed infrastructure
     destroy_result = tf_destroy(ctx, tf_command, tf_directory, var)
-    result_total += destroy_result
 
-    # Output whether the test ran were succesful or failed
-    if result_total == 0:
-        ctx.log("TEST SUCCESSFUL")
+    # Output whether the tests ran were succesful or failed
+    passed_checks = 0
+    failed_checks = 0
+    for check in [init_result, apply_result, destroy_result]:
+        if check == 0:
+            passed_checks += 1
+        else:
+            failed_checks += 1
+
+    ctx.log(Fore.WHITE + "RESULTS:")
+    ctx.log(Fore.WHITE + "-------------------------\n")
+
+    ctx.log(
+        Fore.WHITE
+        + "Passed checks: {0}, Failed checks: {1}\n".format(
+            passed_checks, failed_checks
+        )
+    )
+
+    if init_result == 0:
+        ctx.log("INIT -- SUCCESS")
     else:
-        ctx.log("TEST FAILED", level="error")
+        ctx.log("INIT -- FAILED", level="error")
+    if apply_result == 0:
+        ctx.log("APPLY -- SUCCESS", level="warning")
+    else:
+        ctx.log("APPLY -- FAILED", level="error")
+    if destroy_result == 0:
+        ctx.log("DESTROY -- SUCCESS")
+    else:
+        ctx.log("DESTROY -- FAILED", level="error")
